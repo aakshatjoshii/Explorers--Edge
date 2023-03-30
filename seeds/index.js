c4929b2f81053499cac2ca343e8f3c46ba5eef92
aakshatjoshii <92366113+aakshatjoshii@users.noreply.github.com>
2023-03-31 01:39:21 +0530

    Basic Styling-ReRouting

--- seeds/index.js
+++ seeds/index.js
@@ -20,19 +20,27 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
+            // user id
             author: '641a1a2adce67af9b2185d7b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi expedita reiciendis commodi quia ut eum optio architecto, labore magni! Unde fugit officiis ex. At, sit suscipit! Quasi tempore ab placeat.',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images:[
                 {
-                    url: 'https://res.cloudinary.com/dcrgrsnyr/image/upload/v1679514897/SmartTravel/cli20jwgajpgomvelecq.jpg',
-                    filename: 'SmartTravel/cli20jwgajpgomvelecq',
+                    url: 'https://res.cloudinary.com/dcrgrsnyr/image/upload/v1679517517/SmartTravel/gitgdti9qxekdfxtbl3m.jpg',
+                    filename: 'SmartTravel/gitgdti9qxekdfxtbl3m',
                   },
                   {
-                    url: 'https://res.cloudinary.com/dcrgrsnyr/image/upload/v1679514900/SmartTravel/uvj7vmramejmtpavbqwf.jpg',
-                    filename: 'SmartTravel/uvj7vmramejmtpavbqwf',
+                    url: 'https://res.cloudinary.com/dcrgrsnyr/image/upload/v1679517518/SmartTravel/cxhdrt22x4aypkoaaypg.jpg',
+                    filename: 'SmartTravel/cxhdrt22x4aypkoaaypg',
                   }
                  
             ]
