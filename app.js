e9cd8251340ccdc4401e19d1fd695f517a9230f7
aakshatjoshii <92366113+aakshatjoshii@users.noreply.github.com>
2023-04-24 11:56:19 +0530

    Add files via upload
    Style and Re-routing Update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/travel-planner')
+mongoose.connect('mongodb://0.0.0.0:27017/travel-planner')
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
