2ca92fb790a4582677fd44f6c02a90c7aa7990d0
aakshatjoshii <92366113+aakshatjoshii@users.noreply.github.com>
2023-04-24 12:03:19 +0530

    Add files via upload
    Integrated Travel Advisor api and Google Maps Api

---
+++ CrowdSourcing/pages/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const url =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": "af49e24cb4msh2102c21b8daab9fp11c501jsn7e7a876bbba8",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(`Fetch data Error : ${error}`);
+  }
+};
